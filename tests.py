debdf3409f31b898bf0cfb0bf54ad3f84eea7bc6
Leonid <fortress17.99@mail.ru>
2024-02-29 20:20:31 +0300

    commit_new_2

--- tests.py
+++ tests.py
@@ -8,28 +8,21 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        assert len(collector.books_genre) == 2
+        assert len(collector.get_books_genre()) == 2
 
     def test_add_new_book_name_exceeds_41_characters(self):
         collector = BooksCollector()
 
         collector.add_new_book('Слишком длинное имя книги, чтобы проходило проверку')
 
-        assert 'Слишком длинное имя книги, чтобы проходило проверку' not in collector.books_genre
+        assert 'Слишком длинное имя книги, чтобы проходило проверку' not in collector.get_books_genre()
 
     def test_set_book_genre_book_and_genre_specified(self):
         collector = BooksCollector()
 
         collector.add_new_book('Солярис')
         collector.set_book_genre('Солярис', 'Фантастика')
-        assert collector.books_genre['Солярис'] == 'Фантастика'
-
-    def test_set_book_genre_book_not_defined(self):
-        collector = BooksCollector()
-
-        collector.set_book_genre('Дракула', 'Ужасы')
-
-        assert 'Дракула' not in collector.books_genre
+        assert collector.get_book_genre('Солярис') == 'Фантастика'
 
 
     def test_set_book_genre_genre_not_existing(self):
@@ -38,15 +31,8 @@ class TestBooksCollector:
         collector.add_new_book('Дон Кихот')
         collector.set_book_genre('Дон Кихот', 'Роман')
 
-        assert 'Роман' not in collector.books_genre
+        assert 'Роман' not in collector.get_books_genre()
 
-    def test_get_book_genre(self):
-        collector = BooksCollector()
-
-        collector.add_new_book('Хирург')
-        collector.set_book_genre('Хирург', 'Детективы')
-
-        assert collector.get_book_genre('Хирург') == 'Детективы'
 
     def test_get_books_with_specific_genre(self):
         collector = BooksCollector()
@@ -65,24 +51,20 @@ class TestBooksCollector:
         collector.add_new_book('Хирург')
         collector.set_book_genre('Хирург', 'Детективы')
 
-        assert collector.books_genre == {'Сияние': 'Ужасы', 'Хирург': 'Детективы'}
+        assert collector.get_books_genre() == {'Сияние': 'Ужасы', 'Хирург': 'Детективы'}
 
 
-    def test_get_books_for_children_book_appended_in_books_for_children(self):
+    def test_get_books_for_children_1_book_matches_and_1_is_not(self):
         collector = BooksCollector()
 
         collector.add_new_book('Маша и медведи')
         collector.set_book_genre('Маша и медведи', 'Мультфильмы')
-
-        assert 'Маша и медведи' in collector.get_books_for_children()
-
-    def test_get_books_for_children_book_not_appended_in_books_for_children(self):
-        collector = BooksCollector()
-
         collector.add_new_book('Сияние')
         collector.set_book_genre('Сияние', 'Ужасы')
 
-        assert 'Сияние' not in collector.get_books_for_children()
+        assert 'Маша и медведи' in collector.get_books_for_children() and 'Сияние' not in collector.get_books_for_children()
+
+
 
     def test_book_in_favourites_check_added_in_favourites(self):
         collector = BooksCollector()
@@ -90,7 +72,7 @@ class TestBooksCollector:
         collector.add_new_book('Этюд в багровых тонах')
         collector.add_book_in_favorites('Этюд в багровых тонах')
 
-        assert 'Этюд в багровых тонах' in collector.favorites
+        assert 'Этюд в багровых тонах' in collector.get_list_of_favorites_books()
 
     def test_delete_book_from_favorites_check_deleted_from_favourites(self):
         collector = BooksCollector()
@@ -99,15 +81,7 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Этюд в багровых тонах')
         collector.delete_book_from_favorites('Этюд в багровых тонах')
 
-        assert 'Этюд в багровых тонах' not in collector.favorites
+        assert collector.get_list_of_favorites_books() == []
 
-    def test_get_list_of_favorites_books_two_books_added_in_favourites(self):
-        collector = BooksCollector()
 
-        collector.add_new_book('Солярис')
-        collector.add_book_in_favorites('Солярис')
-        collector.add_new_book('Этюд в багровых тонах')
-        collector.add_book_in_favorites('Этюд в багровых тонах')
-
-        assert collector.favorites == ['Солярис', 'Этюд в багровых тонах']
 
